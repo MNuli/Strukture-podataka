f5561504eaa647388cdf4e7278a1c6030ad2414a
MNuli <racer399@gmail.com>
2012-12-16 00:21:44 -0800

    Create red_polje.h

---
+++ red_polje.h
@@ -0,0 +1,45 @@
+klijentBanke tdatum{
+    int dan, mjesec, godina;
+};
+klijentBanke elem {
+    char pre_ime[35];
+    tdatum datum;
+    float tekuci, devizni;
+    int ai, bi, ci, di;
+};
+klijentBanke red {
+    elem klijent[10000];
+    int front, rear;
+};
+       
+red pomocniRed, salter, brzi, novi;
+int n,placanje = 0;
+typedef red& jedanRed;
+
+int AddOne(int n) {
+    return ((n + 1) % 10000); 
+}   
+elem FrontQ(red& pomocniRed) {
+    if(AddOne(pomocniRed.rear) == pomocniRed.front) {
+        system("pause");
+        exit(0);
+    }
+    return pomocniRed.klijent[pomocniRed.front];
+}     
+void EnQueueQ(elem x, red& pomocniRed) {
+    if(AddOne(AddOne(pomocniRed.rear)) == pomocniRed.front) return;
+    pomocniRed.rear = AddOne(pomocniRed.rear);
+    pomocniRed.klijent[pomocniRed.rear] = x;
+}     
+void DeQueueQ(red& pomocniRed) {
+    if(AddOne(pomocniRed.rear) == pomocniRed.front) return;
+    pomocniRed.front = AddOne(pomocniRed.front);
+}    
+void InitQ(red& pomocniRed) {
+    pomocniRed.front = 0;
+    pomocniRed.rear = 9999;
+}     
+bool IsEmptyQ(red& pomocniRed) {
+    if(AddOne(pomocniRed.rear) == pomocniRed.front) return true;
+    else return false;
+}
